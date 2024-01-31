d69b519c9b2b8fbcd2bc456196c86bb2935af29d
Mistereptil09 <antoniociorba@outlook.com>
2024-01-31 12:23:41 +0100

    insertion d'une boucle while pour que l'utilisateur puisse continuer à utiliser le code

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-choix = input("Veuillez entrer un nombre : ")
-nombre = int(choix)
-print(f"Votre nombre {nombre}")
-for i in range (0, 10):
-    multiple = nombre * (i+1)
-    print(f"{multiple}")
+continuer = True
+while continuer:
+    choix = input("Veuillez entrer un nombre : ")
+    nombre = int(choix)
+    print(f"Votre nombre {nombre}")
+    for i in range(0, 10):
+        multiple = nombre * (i+1)
+        print(f"{multiple}")
+    choix = input("Voullez vous continuer ? (oui pour continuer) : ")
+    choix = choix.lower()
+    if choix == "oui":
+        continuer = True
+    else:
+        continuer = False
+        print("Merci d'avoir essayé")
